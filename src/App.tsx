3c8cbe8f145017b9ef4b8721a8e37f9382a108cb
hadyamizari <117837424+hadya286@users.noreply.github.com>
2024-04-02 12:14:18 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Container, Typography} from '@mui/material'
+import Navbar from './components/NavBar/navbar'
+
+const App = () => {
+  return (
+    <Container>
+      <Typography color='black'>It Works</Typography>
+      <Navbar />
+    </Container>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './navbar.css'
+
+import {Container, Typography} from '@mui/material'
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Typography>navbar</Typography>
+    </Container>
+  )
+}
+export default Navbar
