ce4935c1da2097691b4b7d5556901e20677fe3c4
hadyamizari <117837424+hadya286@users.noreply.github.com>
2024-04-03 10:54:59 +0300

    revert some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
 import {Container, Typography} from '@mui/material'
-import Navbar from './components/NavBar/navbar'
 
 const App = () => {
   return (
     <Container>
       <Typography color='black'>It Works</Typography>
-      <Navbar />
     </Container>
   )
 }
